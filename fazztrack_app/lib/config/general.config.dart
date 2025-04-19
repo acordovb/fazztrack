34ee0187e8e602d5334a900d13e5bae2ff109e1e
Alex Córdova <alexcordova111@gmail.com>
2025-04-19 14:59:00 -0500

    Add configuration classes for application settings

---
+++ fazztrack_app/lib/config/general.config.dart
@@ -0,0 +1,10 @@
+class AppConfig {
+  static final AppLevel appLevel = AppLevel();
+  static const String appVersion = '1.0.0';
+  static const String appName = 'Fazztrack';
+}
+
+class AppLevel {
+  final String admin = 'Administrator';
+  final String user = 'User';
+}
