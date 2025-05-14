d36094c0015c4ff137c141a57284cbeb59c3d87c
Alex Córdova <alexcordova111@gmail.com>
2025-05-13 19:02:47 -0500

    feat: Update iOS and macOS project configurations with CocoaPods integration
    - Added Podfile and Podfile.lock for iOS and macOS to manage dependencies.
    - Updated Runner.xcodeproj and Runner.xcworkspace to include Pods and their configurations.
    - Modified Release.xcconfig and Debug.xcconfig to include Pods configurations.
    - Changed base URL in build.config.dart for local development.
    - Enhanced entitlements for macOS to allow network client access.

--- fazztrack_app/lib/config/build.config.dart
+++ fazztrack_app/lib/config/build.config.dart
@@ -2,5 +2,6 @@ import 'package:fazztrack_app/config/general.config.dart';
 
 class BuildConfig {
   static final String appLevel = AppConfig.appLevel.user;
-  static final String baseUrl = 'http://10.0.2.2:3000/api/v1';
+  // static final String baseUrl = 'http://10.0.2.2:3000/api/v1';
+  static final String baseUrl = 'http://127.0.0.1:3000/api/v1';
 }
