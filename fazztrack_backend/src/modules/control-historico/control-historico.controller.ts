b654765866a006b3ee55d51b2547fde5af29e5c0
Alex Córdova <alexcordova111@gmail.com>
2025-07-01 16:47:18 -0500

    refactor: Remove unused imports and methods from ControlHistoricoController for cleaner code

--- fazztrack_backend/src/modules/control-historico/control-historico.controller.ts
+++ fazztrack_backend/src/modules/control-historico/control-historico.controller.ts
@@ -1,21 +1,6 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  HttpStatus,
-  NotFoundException,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ControlHistoricoService } from './control-historico.service';
-import {
-  ControlHistoricoDto,
-  CreateControlHistoricoDto,
-  UpdateControlHistoricoDto,
-} from './dto';
+import { ControlHistoricoDto } from './dto';
 import { decodeId } from 'src/shared/hashid/hashid.utils';
 
 @Controller('control-historico')
@@ -23,13 +8,6 @@ export class ControlHistoricoController {
   constructor(
     private readonly controlHistoricoService: ControlHistoricoService,
   ) {}
-
-  @Post()
-  create(
-    @Body() createControlHistoricoDto: CreateControlHistoricoDto,
-  ): Promise<ControlHistoricoDto> {
-    return this.controlHistoricoService.create(createControlHistoricoDto);
-  }
 
   @Get('estudiante/:idEstudiante')
   async findByEstudianteId(
@@ -43,21 +21,6 @@ export class ControlHistoricoController {
         id_estudiante: decodeId(idEstudiante),
       });
     }
-
     return controlHistorico;
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateControlHistoricoDto: UpdateControlHistoricoDto,
-  ): Promise<ControlHistoricoDto> {
-    return this.controlHistoricoService.update(id, updateControlHistoricoDto);
-  }
-
-  @Delete(':id')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('id') id: string): Promise<void> {
-    return this.controlHistoricoService.remove(id);
   }
 }
