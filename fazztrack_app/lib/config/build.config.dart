c215a3b361a18057765bc9ac4a050a893db88bf0
Alex Córdova <alexcordova111@gmail.com>
2025-07-16 15:40:36 -0500

    feat: Add environment configuration file and update base URL handling

--- fazztrack_app/lib/config/build.config.dart
+++ fazztrack_app/lib/config/build.config.dart
@@ -1,7 +1,12 @@
 import 'package:fazztrack_app/config/general.config.dart';
+import 'package:fazztrack_app/config/env.config.dart';
 
 class BuildConfig {
   static final String appLevel = AppConfig.appLevel.admin;
-  // static final String baseUrl = 'http://10.0.2.2:3000/api/v1';
-  static final String baseUrl = 'http://127.0.0.1:3000/api/v1';
+
+  // Obtener la URL base desde variables de entorno o archivo local
+  static final String baseUrl = const String.fromEnvironment(
+    'BASE_URL',
+    defaultValue: EnvConfig.baseUrl, // Usa la URL del archivo de entorno
+  );
 }
